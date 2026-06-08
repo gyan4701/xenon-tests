3a5a8548c09d7fa097fa389b1ad830e8b90ba817
gyan4701 <gyan.ranjan@jadeglobal.com>
2026-06-08 14:23:39 +0530

    Update Locator_Heal.spec.js

--- automation-scripts/Playwright/JavaScript/Locator_Heal.spec.js
+++ automation-scripts/Playwright/JavaScript/Locator_Heal.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('SELF HEAL DEMO - Create Salesforce Lead with broken New locator', async ({ page }) => {
-  test.setTimeout(30000);
+  test.setTimeout(60000);
 
   const salesforceOrigin =
     process.env.SF_INSTANCE_URL ||
